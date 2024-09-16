ef02b030ebcf29b08b54fa19614cf37587eca983
theEvilReaper <steffenwx@gmail.com>
2024-09-16 21:41:27 +0200

    Improve Helper class

--- common/src/main/java/net/onelitefeather/cygnus/common/util/Helper.java
+++ common/src/main/java/net/onelitefeather/cygnus/common/util/Helper.java
@@ -3,13 +3,9 @@ package net.onelitefeather.cygnus.common.util;
 import de.icevizion.aves.util.Players;
 import de.icevizion.xerus.api.team.Team;
 import de.icevizion.xerus.api.team.TeamService;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Player;
 import net.minestom.server.entity.attribute.Attribute;
 import net.minestom.server.utils.Direction;
@@ -85,50 +81,6 @@ public final class Helper {
 
     public static int getRandomInt(int maximumValue) {
         return ThreadLocalRandom.current().nextInt(1, maximumValue);
-    }
-
-    public static @NotNull Direction parseDirection(@NotNull String face) {
-        if (face.trim().isEmpty()) return Direction.NORTH;
-
-        Direction direction = null;
-
-        var horizontalValues = Direction.HORIZONTAL;
-
-        for (int i = 0; i < horizontalValues.length && direction == null; i++) {
-            if (!horizontalValues[i].name().equals(face)) continue;
-            direction = horizontalValues[i];
-        }
-        return direction == null ? Direction.NORTH : direction;
-    }
-
-    public static boolean isValidFace(double pitch) {
-        return pitch >= -50 && pitch <= 50;
-    }
-
-    public static @NotNull Direction getInvalidDirection(double pitch) {
-        return pitch > 50 ? Direction.DOWN : Direction.UP;
-    }
-
-    public static @NotNull Component convertPointToComponent(@NotNull Point pos) {
-        return pos instanceof Vec vec ? convertVec(vec) : convertPos((Pos) pos);
-    }
-
-    private static @NotNull Component convertPos(@NotNull Pos pos) {
-        Component[] data = new Component[5];
-        data[0] = Component.text("x: " + pos.blockX());
-        data[1] = Component.text("y: " + pos.blockY());
-        data[2] = Component.text("z: " + pos.blockZ());
-        data[3] = Component.text("yaw: " + pos.yaw());
-        data[4] = Component.text("pitch: " + pos.pitch());
-        return Component.join(JoinConfiguration.arrayLike(), data);
-    }
-
-    private static @NotNull Component convertVec(@NotNull Vec vec) {
-        Component[] data = new Component[3];
-        data[0] = Component.text("x: " + vec.blockX());
-        data[1] = Component.text("y: " + vec.blockY());
-        data[2] = Component.text("z: " + vec.blockZ());
-        return Component.join(JoinConfiguration.arrayLike(), data);
     }
 
     public static void changeSpeedValue(@NotNull Player player, boolean reset) {
