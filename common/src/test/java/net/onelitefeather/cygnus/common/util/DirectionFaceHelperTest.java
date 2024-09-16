b4429621405a1598e25ce752e09c4424de84b124
theEvilReaper <steffenwx@gmail.com>
2024-09-16 21:41:15 +0200

    Add more tests

---
+++ common/src/test/java/net/onelitefeather/cygnus/common/util/DirectionFaceHelperTest.java
@@ -0,0 +1,50 @@
+package net.onelitefeather.cygnus.common.util;
+
+import net.minestom.server.utils.Direction;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionFaceHelperTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = { "NORTH", "SOUTH", "EAST", "WEST" })
+    void testValidDirectionParsing(String face) {
+        Direction parsedDirection = DirectionFaceHelper.parseDirection(face);
+        assertNotNull(parsedDirection);
+        assertNotEquals(Direction.UP, parsedDirection);
+        assertNotEquals(Direction.DOWN, parsedDirection);
+    }
+
+    @Test
+    void testInvalidDirectionParsing() {
+        assertEquals(Direction.NORTH, DirectionFaceHelper.parseDirection("INVALID"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = { -51, 51 })
+    void testValidFace(double pitch) {
+        assertFalse(DirectionFaceHelper.isValidFace(pitch));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = { 51, 100, 200 })
+    void testInvalidFaceGetByPitch(double pitch) {
+        Direction invalidDirection = DirectionFaceHelper.getInvalidDirection(pitch);
+        assertNotNull(invalidDirection);
+        assertNotHorizontal(invalidDirection);
+    }
+
+    /**
+     * Asserts that the given direction is not horizontal.
+     * @param direction the direction to check
+     */
+    private void assertNotHorizontal(@NotNull Direction direction) {
+        for (int i = 0; i < Direction.HORIZONTAL.length; i++) {
+            assertNotEquals(Direction.HORIZONTAL[i], direction);
+        }
+    }
+}
