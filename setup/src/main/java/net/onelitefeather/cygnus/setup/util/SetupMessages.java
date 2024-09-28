b3798cedfe59003df5894045648479697894f146
Joltras <MinnyMouseXD@gmail.com>
2024-09-28 17:47:24 +0200

    Add new constants

--- setup/src/main/java/net/onelitefeather/cygnus/setup/util/SetupMessages.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/util/SetupMessages.java
@@ -1,14 +1,28 @@
 package net.onelitefeather.cygnus.setup.util;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.onelitefeather.cygnus.common.Messages;
 
 public final class SetupMessages {
 
+    public static final Component EMPTY_NAME;
     public static final Component MISSING_MAP_SELECTION;
+    public static final Component NOT_ALLOWED_IN_LOBBY;
+
+    public static final Component DISABLED_PAGE_MODE;
 
     static {
-        MISSING_MAP_SELECTION = Messages.withMini("<red>Please select a map to setup and try the command again");
+        EMPTY_NAME = Messages.withPrefix(Component.text("An empty name is not allowed", NamedTextColor.RED));
+        MISSING_MAP_SELECTION = Messages.withPrefix(
+                Component.text("Please select a map to setup and try the command again", NamedTextColor.RED)
+        );
+        NOT_ALLOWED_IN_LOBBY = Messages.withPrefix(
+                Component.text("The page mode is not available in the lobby setup mode!", NamedTextColor.RED)
+        );
+        DISABLED_PAGE_MODE =  Messages.withPrefix(
+                Component.text("The page mode is now disabled", NamedTextColor.RED)
+        );
     }
 
     private SetupMessages() {
