392cbe21352db1837f5ec6ef28235dcaf7059aa6
Joltras <MinnyMouseXD@gmail.com>
2024-09-28 17:46:40 +0200

    Improve tag usage

--- setup/src/main/java/net/onelitefeather/cygnus/setup/util/SetupTags.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/util/SetupTags.java
@@ -5,6 +5,7 @@ import net.minestom.server.tag.Tag;
 public final class SetupTags {
 
     public static final Tag<Byte> OCCUPIED_TAG = Tag.Byte("blocked");
+    public static final Tag<Integer> SETUP_ID_TAG = Tag.Integer("setup_id").defaultValue(-1);
 
     private SetupTags() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
