e03ae6ffc5834a0825f6dca46c7de0bd57e135b4
theEvilReaper <steffenwx@gmail.com>
2024-10-21 21:40:42 +0200

    Improve code structure and fix stamina update

--- game/src/main/java/net/onelitefeather/cygnus/stamina/SlenderBar.java
+++ game/src/main/java/net/onelitefeather/cygnus/stamina/SlenderBar.java
@@ -3,9 +3,11 @@ package net.onelitefeather.cygnus.stamina;
 import de.icevizion.aves.util.Components;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.Player;
 import net.minestom.server.entity.attribute.Attribute;
+import net.minestom.server.instance.Instance;
 import net.minestom.server.potion.Potion;
 import net.minestom.server.potion.PotionEffect;
 import net.minestom.server.potion.TimedPotion;
@@ -20,10 +22,10 @@ import java.util.function.BiFunction;
 /**
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
 @SuppressWarnings({"java:S3252"})
-public non-sealed class SlenderBar extends StaminaBar {
+public non-sealed class SlenderBar extends StaminaBar implements SlenderBarHelper {
 
     private static final TimedPotion NIGHT_VISION = new TimedPotion(new Potion(PotionEffect.NIGHT_VISION, (byte) 1, -1), 0);
     private static final TimedPotion BLINDNESS = new TimedPotion(new Potion(PotionEffect.BLINDNESS, (byte) 1, -1), 0);
@@ -36,30 +38,32 @@ public non-sealed class SlenderBar extends StaminaBar {
     private static final float TIME_STEP = 0.5f;
     private final String tileChar;
     private final int time;
-    private BiFunction<Player, Status, Void> accept;
+    private BiFunction<Player, State, Void> accept;
     private double currentTime;
+    private StaminarColors colorState;
 
     SlenderBar(@NotNull CygnusPlayer player) {
         super(player, ChronoUnit.MILLIS, 500);
         this.tileChar = "▋";
         this.time = MAX_TIME;
         this.currentTime = time;
+        this.colorState = StaminarColors.DRAINING;
     }
 
-    public void setAccept(@NotNull BiFunction<Player, Status, Void> accept) {
+    public void setAccept(@NotNull BiFunction<Player, State, Void> accept) {
         this.accept = accept;
     }
 
     @Override
     protected void onStart() {
-        this.status = Status.READY;
+        this.state = State.READY;
         this.player.addEffect(NIGHT_VISION.potion());
     }
 
     @Override
     public void consume() {
-        if (status == Status.READY) return;
-        if (status == Status.DRAINING) {
+        if (state == State.READY) return;
+        if (state == State.DRAINING) {
             this.handleDraining();
             return;
         }
@@ -69,59 +73,58 @@ public non-sealed class SlenderBar extends StaminaBar {
     private void handleDraining() {
         if (currentTime >= 0) {
             currentTime -= TIME_STEP;
-            var nearbyEntities = player.getInstance().getNearbyEntities(player.getPosition(), 3);
-
-            if (nearbyEntities.isEmpty()) return;
-
-            for (Entity nearbyEntity : nearbyEntities) {
-                if (nearbyEntity instanceof Player target && !player.getUuid().equals(target.getUuid()) && (target.getHealth() > 0)) {
-                    target.setHealth(target.getHealth() - TIME_STEP);
-                }
-            }
+            Instance instance = player.getInstance();
+            applyDamage(instance, player.getUuid(), Pos.fromPoint(player.getPosition()), 3, TIME_STEP);
             return;
         }
-        status = Status.REGENERATING;
-        this.accept.apply(player, status);
+        state = State.REGENERATING;
+        colorState = StaminarColors.REGENERATING;
+        this.accept.apply(player, state);
         player.setTag(Tags.HIDDEN, (byte) 0);
         player.removeEffect(BLINDNESS.potion().effect());
         player.addEffect(NIGHT_VISION.potion());
+        this.colorState.sendProgressBar(player, tileChar, (int) currentTime);
     }
 
     private void handleRegeneration() {
         if (currentTime < time + TIME_STEP) {
             currentTime += TIME_STEP;
         } else {
-            status = Status.READY;
+            state = State.READY;
+            colorState = StaminarColors.DRAINING;
             player.playSound(LEVEL, player.getPosition());
         }
-
+        this.colorState.sendProgressBar(player, tileChar, (int) currentTime);
         player.sendActionBar(Components.getProgressBar((int) currentTime, 17, 17, tileChar, NamedTextColor.GOLD, NamedTextColor.GRAY));
     }
 
     public boolean changeStatus() {
-        if (status == Status.REGENERATING && this.time <= 10) return false;
-        switch (status) {
+        if (state == State.REGENERATING && this.time <= 10) return false;
+        switch (state) {
             case READY -> {
-                status = Status.DRAINING;
+                state = State.DRAINING;
+                colorState = StaminarColors.DRAINING;
                 player.setTag(Tags.HIDDEN, (byte) 1);
                 player.removeEffect(NIGHT_VISION.potion().effect());
                 player.addEffect(BLINDNESS.potion());
                 player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.1f);
-                this.accept.apply(player, Status.DRAINING);
+                this.accept.apply(player, State.DRAINING);
             }
             case REGENERATING -> {
-                status = Status.DRAINING;
+                state = State.DRAINING;
+                colorState = StaminarColors.DRAINING;
                 player.setTag(Tags.HIDDEN, (byte) 1);
                 playSoundToTarget(false);
-                this.accept.apply(player, Status.DRAINING);
+                this.accept.apply(player, State.DRAINING);
             }
             case DRAINING -> {
-                status = Status.REGENERATING;
+                state = State.REGENERATING;
+                colorState = StaminarColors.REGENERATING;
                 player.setTag(Tags.HIDDEN, (byte) 0);
                 playSoundToTarget(true);
                 player.removeEffect(BLINDNESS.potion().effect());
                 player.addEffect(NIGHT_VISION.potion());
-                this.accept.apply(player, Status.REGENERATING);
+                this.accept.apply(player, State.REGENERATING);
             }
         }
         return true;
@@ -137,9 +140,5 @@ public non-sealed class SlenderBar extends StaminaBar {
                 target.playSound(spawn ? SPAWN : TELEPORT, target.getPosition());
             }
         }
-    }
-
-    private void sendProgressBar() {
-        player.sendActionBar(Components.getProgressBar((int) currentTime, 17, 17, tileChar, NamedTextColor.GREEN, NamedTextColor.GRAY));
     }
 }
