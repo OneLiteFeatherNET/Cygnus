0a7488e11a2d90ced8eedcdd7daa4bc3e057b161
Joltras <MinnyMouseXD@gmail.com>
2024-11-02 17:49:40 +0100

    Add click consumer, Tag to store the ID from the item

--- spectator/src/main/java/net/onelitefeather/spectator/item/SpectatorItem.java
+++ spectator/src/main/java/net/onelitefeather/spectator/item/SpectatorItem.java
@@ -1,7 +1,11 @@
 package net.onelitefeather.spectator.item;
 
+import net.minestom.server.entity.Player;
 import net.minestom.server.item.ItemStack;
+import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
 
 /**
  * The SpectatorItem interface defines the fundamental properties of an {@link ItemStack} that can be used
@@ -10,13 +14,15 @@ import org.jetbrains.annotations.NotNull;
  * A SpectatorItem represents an item shown in the inventory of a spectator, complete with details such as
  * the item’s slot and internal indices.
  * <p>
- * The SpectatorItem interface is sealed and only permits extensions from the {@link SpectatorItemContainer} class.
+ * The SpectatorItem interface is sealed and only permits extensions from the {@link SpectatorHotBarItem} class.
  *
  * @version 1.0.0
- * @see SpectatorItemContainer
+ * @see SpectatorHotBarItem
  * @since 1.0.0
  */
-public sealed interface SpectatorItem permits SpectatorItemContainer {
+public sealed interface SpectatorItem permits SpectatorHotBarItem {
+
+    Tag<Integer> SPEC_ITEM_TAG = Tag.Integer("spectator_item");
 
     /**
      * Retrieves the {@link ItemStack} displayed in the spectator's inventory.
@@ -40,4 +46,11 @@ public sealed interface SpectatorItem permits SpectatorItemContainer {
      * @return an integer representing the inventory slot index
      */
     int slotIndex();
+
+    /**
+     * Retrieves the logic to be executed when the item is clicked by the spectator.
+     *
+     * @return a non-null {@link Consumer} that represents the logic to be executed
+     */
+    @NotNull Consumer<Player> logic();
 }
