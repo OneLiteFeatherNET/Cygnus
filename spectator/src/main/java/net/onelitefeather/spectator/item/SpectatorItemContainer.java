62bb070ab6c82288b112981aeae955bb3d19d6ce
Joltras <MinnyMouseXD@gmail.com>
2024-11-02 17:48:52 +0100

    Add Consumer to represent the click logic

--- spectator/src/main/java/net/onelitefeather/spectator/item/SpectatorItemContainer.java
+++ spectator/src/main/java/net/onelitefeather/spectator/item/SpectatorItemContainer.java
@@ -1,10 +1,13 @@
 package net.onelitefeather.spectator.item;
 
+import net.minestom.server.entity.Player;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.utils.math.IntRange;
 import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
 
 /**
  * The {@link SpectatorItemContainer} is the implementation of the {@link SpectatorItem} interface.
@@ -12,12 +15,14 @@ import org.jetbrains.annotations.NotNull;
  * @param item      the item to be displayed
  * @param itemIndex the index of the item
  * @param slotIndex the index of the slot
+ * @param logic     the logic to be executed when the item is clicked
  */
 @SuppressWarnings("java:S3252")
 public record SpectatorItemContainer(
         @NotNull ItemStack item,
         int itemIndex,
-        int slotIndex
+        int slotIndex,
+        @NotNull Consumer<Player> logic
 ) implements SpectatorItem {
 
     private static final IntRange SLOT_RANGE = new IntRange(0, 8);
