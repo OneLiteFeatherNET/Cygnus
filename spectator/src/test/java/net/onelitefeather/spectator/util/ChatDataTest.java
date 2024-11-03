f821c644d6aeb71fb3a87946ee1bac21767c4fde
theEvilReaper <steffenwx@gmail.com>
2024-11-03 22:28:41 +0100

    Update constructor usage

--- spectator/src/test/java/net/onelitefeather/spectator/util/ChatDataTest.java
+++ spectator/src/test/java/net/onelitefeather/spectator/util/ChatDataTest.java
@@ -1,6 +1,7 @@
 package net.onelitefeather.spectator.util;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,8 +10,9 @@ class ChatDataTest {
 
     @Test
     void testBasicCreation() {
-        ChatData chatData = new ChatData(Component.text("Hello, world!"), Component.empty());
+        ChatData chatData = new ChatData(Component.text("Hello, world!"), Component.empty(), NamedTextColor.GRAY);
         assertNotNull(chatData);
+        assertEquals(NamedTextColor.GRAY, chatData.messageColor());
         assertNotEquals(Component.empty(), chatData.prefix());
         assertEquals(Component.empty(), chatData.separator());
     }
