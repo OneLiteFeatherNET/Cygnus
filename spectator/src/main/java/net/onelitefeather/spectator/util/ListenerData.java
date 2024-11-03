592b362ede2f381346d3778187aa7f8435ba7028
theEvilReaper <steffenwx@gmail.com>
2024-11-03 22:50:36 +0100

    Rename variables

--- spectator/src/main/java/net/onelitefeather/spectator/util/ListenerData.java
+++ spectator/src/main/java/net/onelitefeather/spectator/util/ListenerData.java
@@ -5,23 +5,23 @@ import org.jetbrains.annotations.NotNull;
 /**
  * The {@link ListenerData} is a data class to store listener options for the spectator module.
  *
- * @param detektSpectatorQuit if the quit event should be detected
- * @param detektSpectatorJoin if the join event should be detected
- * @param detektSpectatorChat if the chat event should be detected
+ * @param detectSpectatorQuit if the quit event should be detected
+ * @param detectSpectatorJoin if the join event should be detected
+ * @param detectSpectatorChat if the chat event should be detected
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
  */
-public record ListenerData(boolean detektSpectatorQuit, boolean detektSpectatorJoin, boolean detektSpectatorChat) {
+public record ListenerData(boolean detectSpectatorQuit, boolean detectSpectatorJoin, boolean detectSpectatorChat) {
 
     /**
      * Creates a new instance of the {@link ListenerData} with the given parameters.
      *
-     * @param detektSpectatorQuit if the quit event should be detected
-     * @param detektSpectatorChat if the chat event should be detected
+     * @param detectSpectatorQuit if the quit event should be detected
+     * @param detectSpectatorChat if the chat event should be detected
      * @return a new instance of the {@link ListenerData}
      */
-    public static @NotNull ListenerData of(boolean detektSpectatorQuit, boolean detektSpectatorChat) {
-        return new ListenerData(detektSpectatorQuit, false, detektSpectatorChat);
+    public static @NotNull ListenerData of(boolean detectSpectatorQuit, boolean detectSpectatorChat) {
+        return new ListenerData(detectSpectatorQuit, false, detectSpectatorChat);
     }
 }
