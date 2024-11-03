2499092a7af3b5b4cfcb13fb728edf95fe952c8a
theEvilReaper <steffenwx@gmail.com>
2024-11-03 22:03:34 +0100

    Remove empty constructor

--- spectator/src/main/java/net/onelitefeather/spectator/util/ChatData.java
+++ spectator/src/main/java/net/onelitefeather/spectator/util/ChatData.java
@@ -1,7 +1,6 @@
 package net.onelitefeather.spectator.util;
 
 import net.kyori.adventure.text.Component;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -12,14 +11,4 @@ import org.jetbrains.annotations.NotNull;
  * @since 1.0.0
  */
 public record ChatData(@NotNull Component prefix, @NotNull Component separator) {
-
-    /**
-     * Creates a empty chat data instance.
-     *
-     * @return a new instance of the chat data
-     */
-    @Contract(pure = true)
-    public static @NotNull ChatData empty() {
-        return new ChatData(Component.empty(), Component.empty());
-    }
 }
