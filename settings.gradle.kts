6001d7ba27699214fa37d4581ae0289e6aefc0c4
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-09-06 23:06:04 +0200

    chore(settings): rework repositories usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,47 +7,26 @@ pluginManagement {
     }
 }
 
-
 dependencyResolutionManagement {
-    if (System.getenv("CI") != null) {
-        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                val ciApiv4Url = System.getenv("CI_API_V4_URL")
-                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Job-Token"
-                    value = System.getenv("CI_JOB_TOKEN")
+    repositories {
+        mavenCentral()
+        maven("https://reposilite.atlasengine.ca/public")
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
                 }
+            } else {
+                credentials(PasswordCredentials::class)
                 authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-        }
-    } else {
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            mavenCentral()
-            maven("https://jitpack.io")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name =  "Private-Token"
-                    value = providers.gradleProperty("gitLabPrivateToken").get()
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
+                    create<BasicAuthentication>("basic")
                 }
             }
         }
     }
-
     versionCatalogs {
         create("libs") {
             version("publishdata", "1.4.0")
