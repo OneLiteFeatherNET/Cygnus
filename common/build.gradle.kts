d925dd8bda2d792461165884069a5f022ba503e2
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-09-08 11:07:29 +0200

    chore(dependencies): add missing dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -14,6 +14,7 @@ java {
 dependencies {
     implementation(platform(libs.mycelium.bom))
     implementation(platform(libs.aonyx.bom))
+    compileOnly(libs.adventure)
     compileOnly(libs.minestom)
     compileOnly(libs.aves)
     compileOnly(libs.xerus)
@@ -23,5 +24,7 @@ dependencies {
     testImplementation(libs.aves)
     testImplementation(libs.xerus)
     testImplementation(libs.junit.api)
+    testImplementation(libs.junit.params)
+    testImplementation(libs.junit.platform.launcher)
     testRuntimeOnly(libs.junit.engine)
 }
