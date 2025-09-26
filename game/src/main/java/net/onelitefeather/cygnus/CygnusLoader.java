081dbd69374ced57c769752949a2fef39f8b41b6
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-09-26 17:53:50 +0200

    feature(cloudnet): add minestom hook

--- game/src/main/java/net/onelitefeather/cygnus/CygnusLoader.java
+++ game/src/main/java/net/onelitefeather/cygnus/CygnusLoader.java
@@ -1,5 +1,8 @@
 package net.onelitefeather.cygnus;
 
+import dev.derklaro.aerogel.Injector;
+import eu.cloudnetservice.driver.inject.InjectionLayer;
+import eu.cloudnetservice.modules.bridge.impl.platform.minestom.MinestomBridgeExtension;
 import net.minestom.server.MinecraftServer;
 
 public final class CygnusLoader {
@@ -7,6 +10,9 @@ public final class CygnusLoader {
     public static void main(String[] args) {
         MinecraftServer server = MinecraftServer.init();
         new Cygnus();
+        try (InjectionLayer<Injector> layer = InjectionLayer.ext()) {
+            layer.instance(MinestomBridgeExtension.class).onLoad();
+        }
         server.start("0.0.0.0", 25565);
     }
 }
