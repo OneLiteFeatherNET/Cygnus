e4b24082c45d1c0730f1685593db77f124ff99ed
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-04-19 22:22:54 +0200

    feat(map): add package-info file and change annotation usage

--- common/src/main/java/net/onelitefeather/cygnus/common/map/MapEntry.java
+++ common/src/main/java/net/onelitefeather/cygnus/common/map/MapEntry.java
@@ -1,19 +1,18 @@
 package net.onelitefeather.cygnus.common.map;
 
 import net.onelitefeather.cygnus.common.config.GameConfig;
-import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 // Maps as path
-public record MapEntry(@NotNull Path path) {
+public record MapEntry(Path path) {
 
     public boolean hasMapFile() {
         return Files.exists(path.resolve(GameConfig.MAP_FILE_NAME));
     }
 
-    public @NotNull Path getMapFile() {
+    public Path getMapFile() {
         return path.resolve(GameConfig.MAP_FILE_NAME);
     }
 }
