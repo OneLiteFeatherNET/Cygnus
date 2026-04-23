a5c04965957723886fba16450bec382b4ac6cbab
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-04-23 17:15:50 +0200

    docs(component): add missing class documentation

--- game/src/main/java/net/onelitefeather/cygnus/component/TeamNameComponent.java
+++ game/src/main/java/net/onelitefeather/cygnus/component/TeamNameComponent.java
@@ -2,5 +2,13 @@ package net.onelitefeather.cygnus.component;
 
 import net.theevilreaper.xerus.api.component.ObjectComponent;
 
+/**
+ * The {@link TeamNameComponent} is a component that contains the name of the team.
+ *
+ * @param teamName of the team
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public record TeamNameComponent(String teamName) implements ObjectComponent {
 }
