7da4d76b69dbe0e6b92318a971b8957f7c3e07b8
Joltras <minnymousexd@gmail.com>
2026-04-27 14:32:48 +0200

    feat(setup): add position context

--- setup/src/main/java/net/onelitefeather/cygnus/setup/event/dialog/DialogContext.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/event/dialog/DialogContext.java
@@ -1,5 +1,7 @@
 package net.onelitefeather.cygnus.setup.event.dialog;
 
+import net.minestom.server.coordinate.Pos;
+
 /**
  * Represents the context for a dialog event which contains all necessary data
  * to open the correct dialog for a specific {@link DialogTarget}.
@@ -9,8 +11,7 @@ package net.onelitefeather.cygnus.setup.event.dialog;
  * @see DialogTarget
  * @since 0.1.0
  */
-public sealed interface DialogContext permits
-        DialogContext.NameContext {
+public sealed interface DialogContext permits DialogContext.NameContext, DialogContext.PositionContext {
 
     /**
      * Specific context for the update or deletion of a name.
@@ -18,6 +19,10 @@ public sealed interface DialogContext permits
      * @param name of the map
      */
     record NameContext(String name) implements DialogContext {
+
+    }
+
+    record PositionContext(Pos pos) implements DialogContext {
 
     }
 }
