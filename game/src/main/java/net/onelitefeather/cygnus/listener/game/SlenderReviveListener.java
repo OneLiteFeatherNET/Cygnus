6aca5e5a32ce9116d939ee51209ad59670d8abc4
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-05-06 09:50:45 +0200

    chore(stamina): update stamina update call and rename class

--- game/src/main/java/net/onelitefeather/cygnus/listener/game/SlenderReviveListener.java
+++ game/src/main/java/net/onelitefeather/cygnus/listener/game/SlenderReviveListener.java
@@ -1,5 +1,6 @@
 package net.onelitefeather.cygnus.listener.game;
 
+import net.minestom.server.entity.Player;
 import net.onelitefeather.cygnus.common.Tags;
 import net.onelitefeather.cygnus.common.map.GameMap;
 import net.onelitefeather.cygnus.common.util.Helper;
@@ -10,24 +11,26 @@ import net.onelitefeather.cygnus.utils.Items;
 import java.util.function.Consumer;
 
 /**
+ * This class is responsible for handling the {@link SlenderReviveEvent} and performing the necessary actions to revive the player in the game.
+ *
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
-public class GameReviveListener implements Consumer<SlenderReviveEvent> {
+public class SlenderReviveListener implements Consumer<SlenderReviveEvent> {
 
     private final GameMap gameMap;
     private final StaminaService staminaService;
 
-    public GameReviveListener(GameMap gameMap, StaminaService staminaService) {
+    public SlenderReviveListener(GameMap gameMap, StaminaService staminaService) {
         this.gameMap = gameMap;
         this.staminaService = staminaService;
     }
 
     @Override
     public void accept(SlenderReviveEvent event) {
-        var player = event.getPlayer();
-        staminaService.switchToSlenderBar(player);
+        Player player = event.getPlayer();
+        staminaService.setSlenderBar(player, true);
         player.setTag(Tags.GAME_TAG, Helper.SLENDER_ID);
         player.teleport(gameMap.getSlenderSpawn());
         Items.setSlenderEye(player);
