b07096f3dbc231b39ce274bf1196be914f3f94a1
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-05-07 17:45:53 +0200

    feat(page): add PageDiscoveryCompletedEvent

---
+++ common/src/main/java/net/onelitefeather/cygnus/common/page/event/PageDiscoveryCompletedEvent.java
@@ -0,0 +1,13 @@
+package net.onelitefeather.cygnus.common.page.event;
+
+import net.minestom.server.event.Event;
+
+/**
+ * Event will be fired when each available page has been discovered by the players.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 2.3.1
+ */
+public class PageDiscoveryCompletedEvent implements Event {
+}
