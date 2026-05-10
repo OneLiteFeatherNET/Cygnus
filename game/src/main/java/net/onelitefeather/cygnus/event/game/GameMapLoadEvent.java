57497ed0e5f65fa2670a5b40a1f17544b46bb558
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-05-10 11:11:05 +0200

    feat(game): add new events for the game flow

---
+++ game/src/main/java/net/onelitefeather/cygnus/event/game/GameMapLoadEvent.java
@@ -0,0 +1,13 @@
+package net.onelitefeather.cygnus.event.game;
+
+import net.minestom.server.event.Event;
+
+/**
+ * Will be used to indicate that the map for the game should be loaded.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 2.3.1
+ */
+public class GameMapLoadEvent implements Event {
+}
---
+++ game/src/main/java/net/onelitefeather/cygnus/event/stamina/StaminaCreateEvent.java
@@ -0,0 +1,13 @@
+package net.onelitefeather.cygnus.event.stamina;
+
+import net.minestom.server.event.Event;
+
+/**
+ * Will be used to indicate that a stamina bar should be created.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 2.3.1
+ */
+public class StaminaCreateEvent implements Event {
+}
