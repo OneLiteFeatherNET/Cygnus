bf77bfea242b33ec55679bcab563c0c5aa8bf779
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-06-27 07:42:19 +0200

    chore(setup): update slot handling

--- setup/src/main/java/net/onelitefeather/cygnus/setup/inventory/view/LobbyOverviewInventory.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/inventory/view/LobbyOverviewInventory.java
@@ -3,18 +3,17 @@ package net.onelitefeather.cygnus.setup.inventory.view;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.entity.Player;
 import net.minestom.server.inventory.InventoryType;
-import net.minestom.server.inventory.click.Click;
-import net.minestom.server.item.ItemStack;
+import net.onelitefeather.cygnus.setup.inventory.slot.MultiStringSlot;
+import net.onelitefeather.cygnus.setup.inventory.slot.PositionSlot;
+import net.onelitefeather.cygnus.setup.inventory.slot.StringSlot;
 import net.onelitefeather.cygnus.setup.item.SetupItems;
 import net.onelitefeather.cygnus.setup.map.MapDataCategory;
-import net.onelitefeather.cygnus.setup.util.SetupTags;
 import net.theevilreaper.aves.inventory.InventoryLayout;
 import net.theevilreaper.aves.inventory.PersonalInventoryBuilder;
-import net.theevilreaper.aves.inventory.click.ClickHolder;
+import net.theevilreaper.aves.inventory.slot.ISlot;
 import net.theevilreaper.aves.inventory.util.LayoutCalculator;
 import net.theevilreaper.aves.map.BaseMapBuilder;
 
-import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 public class LobbyOverviewInventory extends PersonalInventoryBuilder {
@@ -40,7 +39,7 @@ public class LobbyOverviewInventory extends PersonalInventoryBuilder {
 
             for (int i = 0; i < categories.length && i < DATA_SLOT.length; i++) {
                 MapDataCategory currentType = categories[i];
-                dataLayout.setItem(DATA_SLOT[i], getDataItem(currentType), this::handleClick);
+                dataLayout.setItem(DATA_SLOT[i], getDataSlot(currentType));
             }
             return dataLayout;
         });
@@ -48,39 +47,18 @@ public class LobbyOverviewInventory extends PersonalInventoryBuilder {
         register();
     }
 
-    private void handleClick(Player player, int slot, Click click, ItemStack stack, Consumer<ClickHolder> consumer) {
-        consumer.accept(ClickHolder.cancelClick());
-
-        MapDataCategory category = stack.getTag(SetupTags.MAP_DATA_CATEGORY_TAG);
-
-        if (click instanceof Click.Left) {
-
-        }
-    }
-
     /**
-     * Returns the {@link ItemStack} for the given category.
+     * Returns the slot for the given category.
      *
-     * @param category the category to get the stack for
-     * @return the stack for the given category
+     * @param category to get the slot for
+     * @return the slot for the given category
      */
-    private ItemStack getDataItem(MapDataCategory category) {
+    private ISlot getDataSlot(MapDataCategory category) {
         return switch (category) {
-            case NAME -> resolveItem(category, () -> this.mapBuilder.getName().equals("Map"));
-            case AUTHOR -> resolveItem(category, () -> this.mapBuilder.getBuilders().isEmpty());
-            case SPAWN -> resolveItem(category, () -> this.mapBuilder.getSpawn() == null);
-            default -> ItemStack.AIR;
+            case NAME -> new StringSlot(MapDataCategory.NAME, mapBuilder.getName());
+            case AUTHOR -> new MultiStringSlot(MapDataCategory.AUTHOR, mapBuilder.getBuilders());
+            case SPAWN -> new PositionSlot(MapDataCategory.SPAWN, mapBuilder.getSpawn());
+            default -> throw new IllegalStateException("Unknown category: " + category);
         };
-    }
-
-    /**
-     * Resolves the item for the given category based on the given condition.
-     *
-     * @param category  which category to resolve
-     * @param condition the condition to check
-     * @return the item for the given category
-     */
-    private ItemStack resolveItem(MapDataCategory category, BooleanSupplier condition) {
-        return condition.getAsBoolean() ? MapDataCategory.getDefaultItem(category) : ItemStack.AIR;
     }
 }
