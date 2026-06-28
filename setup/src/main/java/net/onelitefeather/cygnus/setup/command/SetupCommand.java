9b6bdb2a7713e64d67cb64b64f5a11cf4ee84dca
Joltras <minnymousexd@gmail.com>
2026-06-28 15:57:35 +0200

    chore(setup): remove builders and page mode command

--- setup/src/main/java/net/onelitefeather/cygnus/setup/command/SetupCommand.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/command/SetupCommand.java
@@ -5,8 +5,6 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.condition.Conditions;
 import net.onelitefeather.cygnus.common.Messages;
-import net.onelitefeather.cygnus.setup.command.parts.SetupBuildersCommand;
-import net.onelitefeather.cygnus.setup.command.parts.SetupPageCommand;
 import net.onelitefeather.cygnus.setup.command.parts.SetupSlenderSpawnCommand;
 import net.onelitefeather.cygnus.setup.command.parts.SetupSurvivorSpawnCommand;
 import net.onelitefeather.cygnus.setup.util.SetupData;
