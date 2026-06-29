0f76ed7fbc43fb2d2d28ca3ea3f4d124d6a18126
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2026-06-29 10:44:34 +0200

    fix(release): Replace semtaic releases (#107)
    * chore(release): remove GitHub Actions workflow and semantic-release configuration
    
    * chore(build): integrate release-please and streamline workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
     alias(libs.plugins.cyclonedx)
 }
+
+allprojects {
+    group = "net.onelitefeather"
+    version = providers.gradleProperty("version").get().substringBefore("#").trim()
+}
