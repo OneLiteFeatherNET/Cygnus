b79d4e69b4e051732632eb9d85162494207eb67e
Joltras <minnymousexd@gmail.com>
2026-06-29 13:45:56 +0200

    fix(category): avoid typo in one enum entry declaration

--- setup/src/main/java/net/onelitefeather/cygnus/setup/map/MapDataCategory.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/map/MapDataCategory.java
@@ -9,13 +9,20 @@ import net.onelitefeather.cygnus.setup.util.SetupTags;
 import java.util.EnumMap;
 import java.util.Map;
 
+/**
+ * Enumeration of each possible map data which is required for the setup.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 2.5.0
+ */
 public enum MapDataCategory {
 
     NAME("Name", Material.ACACIA_SIGN, NamedTextColor.YELLOW),
     AUTHOR("Builder", Material.DARK_OAK_DOOR, NamedTextColor.AQUA),
     SPAWN("Spawn", Material.COMPASS, NamedTextColor.RED),
     SLENDER("Slender", Material.ENDER_EYE, NamedTextColor.DARK_PURPLE),
-    SURVIVOR("Surivior", Material.CLOCK, NamedTextColor.GREEN),
+    SURVIVOR("Survivor", Material.CLOCK, NamedTextColor.GREEN),
     PAGE("Page", Material.PAPER, NamedTextColor.WHITE);
 
     private static final MapDataCategory[] VALUES = values();
@@ -25,26 +32,44 @@ public enum MapDataCategory {
     private final Material material;
     private final NamedTextColor color;
 
+    /**
+     * Creates a new instance of the enumeration with the given values.
+     *
+     * @param name     of the entry
+     * @param material of the entry
+     * @param color    of the entry
+     */
     MapDataCategory(String name, Material material, NamedTextColor color) {
         this.name = name;
         this.material = material;
         this.color = color;
     }
 
+    /**
+     * Returns the custom name of the enumeration entry.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the given material of the entry.
+     *
+     * @return the material
+     */
     public Material getMaterial() {
         return material;
     }
 
+    /**
+     * Returns the given color value of the entry.
+     *
+     * @return the color
+     */
     public NamedTextColor getColor() {
         return color;
-    }
-
-    public static MapDataCategory[] getValues() {
-        return VALUES;
     }
 
     /**
@@ -61,8 +86,15 @@ public enum MapDataCategory {
                         .build());
     }
 
+    /**
+     * Try to parse a given id to a {@link MapDataCategory}.
+     *
+     * @param id to get the entry
+     * @return the corresponding enum entry
+     * @throws IllegalArgumentException when the provided id is valid
+     */
     public static MapDataCategory byId(int id) {
-        if  (id < 0 || id >= VALUES.length) {
+        if (id < 0 || id >= VALUES.length) {
             throw new IllegalArgumentException("Invalid map data category ID: " + id);
         }
         return VALUES[id];
