1a6dc04efa0308cb791627a7d31f3f8891fefbdd
Joltras <minnymousexd@gmail.com>
2026-06-29 13:27:38 +0200

    feat(setup): add lore lines to each used item in the hotbar

--- setup/src/main/java/net/onelitefeather/cygnus/setup/item/SetupItems.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/item/SetupItems.java
@@ -6,7 +6,13 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.onelitefeather.cygnus.common.Tags;
+import net.onelitefeather.cygnus.setup.util.SetupMessages;
 import net.theevilreaper.aves.hotbar.HotBarLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.onelitefeather.cygnus.setup.util.SetupMessages.SPACE_SEPARATOR;
 
 /**
  * The class holds the {@link ItemStack} references which have some functionality during a setup process from an map.
@@ -19,8 +25,8 @@ import net.theevilreaper.aves.hotbar.HotBarLayout;
 public final class SetupItems {
 
     public static final ItemStack DECORATION_PANE;
-    public static final byte DEFAULT    = (byte) 0x00;
-    public static final byte SELECTION  = (byte) 0x04;
+    public static final byte DEFAULT = (byte) 0x00;
+    public static final byte SELECTION = (byte) 0x04;
     private static final HotBarLayout selectionLayout;
     private static final HotBarLayout lobbySetupLayout;
     private static final HotBarLayout gameSetupLayout;
@@ -34,12 +40,14 @@ public final class SetupItems {
 
         ItemStack saveItem = ItemStack.builder(Material.BELL)
                 .customName(Component.text("Save data", NamedTextColor.RED))
+                .lore(getLore(SPACE_SEPARATOR.append(Component.text("Save your current progress of the map", NamedTextColor.WHITE))))
                 .set(Tags.ITEM_TAG, SetupItemId.SAVE_DATA)
                 .build();
 
         selectionLayout = new HotBarLayout();
         selectionLayout.set(SELECTION, ItemStack.builder(Material.CHEST)
                 .customName(Component.text("Map selection", NamedTextColor.GREEN))
+                .lore(getLore(SPACE_SEPARATOR.append(Component.text("Chose your map to setup", NamedTextColor.WHITE))))
                 .set(Tags.ITEM_TAG, SetupItemId.MAP_SELECTION)
                 .build()
         );
@@ -47,6 +55,7 @@ public final class SetupItems {
         lobbySetupLayout = new HotBarLayout();
         lobbySetupLayout.set(2, ItemStack.builder(Material.COMPASS)
                 .customName(Component.text("Data", NamedTextColor.AQUA))
+                .lore(getLore(SPACE_SEPARATOR.append(Component.text("View basic map data", NamedTextColor.WHITE))))
                 .set(Tags.ITEM_TAG, SetupItemId.DATA)
                 .build()
         );
@@ -55,16 +64,35 @@ public final class SetupItems {
         gameSetupLayout = new HotBarLayout();
         gameSetupLayout.set(1, ItemStack.builder(Material.COMPASS)
                 .customName(Component.text("Data", NamedTextColor.AQUA))
+                .lore(getLore(SPACE_SEPARATOR.append(Component.text("View basic map data", NamedTextColor.WHITE))))
                 .set(Tags.ITEM_TAG, SetupItemId.DATA)
                 .build()
         );
         gameSetupLayout.set(3, ItemStack.builder(Material.PAPER)
                 .customName(Component.text("Page", NamedTextColor.AQUA))
+                .lore(
+                        getLore(
+                                SPACE_SEPARATOR
+                                        .append(Component.text("Switch to the ", NamedTextColor.WHITE)
+                                                .append(Component.text("Page ", NamedTextColor.AQUA))
+                                                .append(Component.text("setup mode", NamedTextColor.WHITE))
+                                        )
+                        )
+                )
                 .set(Tags.ITEM_TAG, SetupItemId.PAGE)
                 .build()
         );
+
         gameSetupLayout.set(5, ItemStack.builder(Material.MINECART)
-                .customName(Component.text("Survivor", NamedTextColor.YELLOW))
+                .customName(Component.text("Survivor", NamedTextColor.GREEN))
+                .lore(
+                        getLore(
+                                SPACE_SEPARATOR
+                                        .append(Component.text("Switch to the ", NamedTextColor.WHITE)
+                                                .append(Component.text("Survivor ", NamedTextColor.GREEN))
+                                                .append(Component.text("setup mode", NamedTextColor.WHITE)))
+                        )
+                )
                 .set(Tags.ITEM_TAG, SetupItemId.SURVIVOR)
                 .build()
         );
@@ -72,20 +100,40 @@ public final class SetupItems {
 
         pageLayout = new HotBarLayout();
         pageLayout.set(4, ItemStack.builder(Material.BARRIER)
-                .customName(Component.text("Leave page mode", NamedTextColor.RED))
+                .customName(Component.text("Leave mode", NamedTextColor.RED))
+                .lore(
+                        getLore(
+                                SPACE_SEPARATOR.append(
+                                        Component.text("Exit the current mode", NamedTextColor.WHITE)
+                                )
+                        )
+                )
                 .set(Tags.ITEM_TAG, SetupItemId.LEAVE_PAGE)
                 .build()
         );
 
         survivorSpawnLayout = new HotBarLayout();
         survivorSpawnLayout.set(2, ItemStack.builder(Material.CHEST)
-                .customName(Component.text("Spawns", NamedTextColor.AQUA))
+                        .customName(Component.text("Spawns", NamedTextColor.AQUA))
+                        .lore(
+                                getLore(SPACE_SEPARATOR
+                                        .append(Component.text("View all available ", NamedTextColor.WHITE))
+                                        .append(Component.text("Survivor ", NamedTextColor.GREEN))
+                                        .append(Component.text("spawns", NamedTextColor.WHITE)))
+                )
                 .set(Tags.ITEM_TAG, SetupItemId.SPAWNS)
                 .build()
         );
         survivorSpawnLayout.set(6, ItemStack.builder(Material.BARRIER)
                 .customName(Component.text("Leave mode", NamedTextColor.RED))
                 .set(Tags.ITEM_TAG, SetupItemId.LEAVE_MODE)
+                .lore(
+                        getLore(
+                                SPACE_SEPARATOR.append(
+                                        Component.text("Exit the current mode", NamedTextColor.WHITE)
+                                )
+                        )
+                )
                 .build()
         );
     }
@@ -140,5 +188,14 @@ public final class SetupItems {
         player.setHeldItemSlot(DEFAULT);
     }
 
-    private SetupItems() {}
+    private SetupItems() {
+    }
+
+    private static List<Component> getLore(Component... components) {
+        List<Component> lore = new ArrayList<>();
+        lore.add(Component.empty());
+        lore.addAll(List.of(components));
+        lore.add(Component.empty());
+        return lore;
+    }
 }
