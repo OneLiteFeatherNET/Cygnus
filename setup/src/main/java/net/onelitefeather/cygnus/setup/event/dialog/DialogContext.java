7611cab5e5c926c503066588285ad1fcbabbe300
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-06-30 14:07:30 +0200

    feat(setup): add new context implementation

--- setup/src/main/java/net/onelitefeather/cygnus/setup/event/dialog/DialogContext.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/event/dialog/DialogContext.java
@@ -1,20 +1,22 @@
 package net.onelitefeather.cygnus.setup.event.dialog;
 
 import net.minestom.server.coordinate.Point;
+import net.onelitefeather.cygnus.common.page.PageResource;
 
 /**
  * Represents the context for a dialog event which contains all necessary data
  * to open the correct dialog for a specific {@link DialogTarget}.
  *
  * @author Joltra
- * @version 1.0.0
+ * @version 1.5.0
  * @see DialogTarget
  * @since 0.1.0
  */
 public sealed interface DialogContext permits
         DialogContext.NameContext,
         DialogContext.PositionContent,
-        DialogContext.AuthorAmount {
+        DialogContext.AuthorAmount,
+        DialogContext.PageContent {
 
     /**
      * Specific context for the update or deletion of a name.
@@ -34,5 +36,21 @@ public sealed interface DialogContext permits
 
     }
 
-    record AuthorAmount(float amount) implements DialogContext {}
+    /**
+     * Specific context to indicate how many authors a map should have.
+     *
+     * @param amount of author
+     */
+    record AuthorAmount(float amount) implements DialogContext {
+
+    }
+
+    /**
+     * Context implementation to bind a {@link PageResource} to a dialog.
+     *
+     * @param resource to bind
+     */
+    record PageContent(PageResource resource) implements DialogContext {
+
+    }
 }
