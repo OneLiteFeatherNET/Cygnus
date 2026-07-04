ee0fbaeeb32180d7dbe7b1a876ae1268d03e5091
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-07-04 15:48:11 +0200

    feat(game): add GamePrepareEvent

---
+++ game/src/main/java/net/onelitefeather/cygnus/map/event/GamePrepareEvent.java
@@ -0,0 +1,13 @@
+package net.onelitefeather.cygnus.map.event;
+
+import net.minestom.server.event.Event;
+
+/**
+ * Event is used in the game to indicate that the game is about to start and some things must be loaded.
+ *
+ * @version 1.0.0
+ * @author theEvilReaper
+ * @since 2.6.6
+ */
+public class GamePrepareEvent implements Event {
+}
