f226f8b5a1c82f4b9cee50314e6676e1fc778e83
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-07-04 15:57:21 +0200

    chore(setup): add page view item and call logic

--- setup/src/main/java/net/onelitefeather/cygnus/setup/data/GameData.java
+++ setup/src/main/java/net/onelitefeather/cygnus/setup/data/GameData.java
@@ -1,6 +1,7 @@
 package net.onelitefeather.cygnus.setup.data;
 
 import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
@@ -117,7 +118,6 @@ public class GameData extends InstanceSetupData {
      */
     @Override
     public void updateTitle() {
-        System.out.println("TItle is " + (title == null));
         if (getMapBuilder().getName().equalsIgnoreCase("Map")) {
             this.title = null;
             super.updateTitle();
@@ -193,6 +193,11 @@ public class GameData extends InstanceSetupData {
         if (SetupItemId.LEAVE_MODE == tagValue) {
             this.swapSurvivorMode();
             SetupItems.setGameLayout(player);
+            return;
+        }
+
+        if (SetupItemId.PAGES == tagValue) {
+            this.openInventory(InventoryTarget.PAGE);
             return;
         }
 
