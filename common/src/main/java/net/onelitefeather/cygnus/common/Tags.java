5672a76f5b0ae2ff1b9e8c2b3b7d40b4f99c09ce
Joltras <minnymousexd@gmail.com>
2026-07-06 13:11:57 +0200

    chore(common): sort tags by their type

--- common/src/main/java/net/onelitefeather/cygnus/common/Tags.java
+++ common/src/main/java/net/onelitefeather/cygnus/common/Tags.java
@@ -8,16 +8,16 @@ import java.util.UUID;
  * The {@link Tags} class contains all tags used in the project. It is a utility class and should not be instantiated.
  *
  * @author theEvilReaper
- * @version 1.0.0
+ * @version 1.0.1
  * @since 1.0.0
  **/
 public final class Tags {
 
-    public static final Tag<Byte> ITEM_TAG = Tag.Byte("itemTag");
     public static final Tag<UUID> PAGE_TAG = Tag.UUID("page");
+
+    public static final Tag<Byte> ITEM_TAG = Tag.Byte("itemTag");
     public static final Tag<Byte> GAME_TAG = Tag.Byte("slender");
     public static final Tag<Byte> TEAM_ID = Tag.Byte("team");
-
     public static final Tag<Byte> HIDDEN = Tag.Byte("hidden");
 
     private Tags() {
