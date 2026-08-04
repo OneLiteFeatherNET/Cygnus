715c9623d35ed65f0dc5a26bb905ea62f3e19e38
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-04 20:30:47 +0200

    chore(game): use manager in the tick loop

--- game/src/main/java/net/onelitefeather/cygnus/listener/player/CygnusPlayerTickListener.java
+++ game/src/main/java/net/onelitefeather/cygnus/listener/player/CygnusPlayerTickListener.java
@@ -2,15 +2,24 @@ package net.onelitefeather.cygnus.listener.player;
 
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.PlayerTickEvent;
+import net.onelitefeather.cygnus.jumpscare.JumpScareManager;
 import net.onelitefeather.cygnus.player.CygnusPlayer;
 
 import java.util.function.Consumer;
 
 public final class CygnusPlayerTickListener implements Consumer<PlayerTickEvent> {
 
+    private final JumpScareManager jumpscareManager;
+
+    public CygnusPlayerTickListener(JumpScareManager jumpscareManager) {
+        this.jumpscareManager = jumpscareManager;
+    }
+
     @Override
     public void accept(PlayerTickEvent event) {
         Player player = event.getPlayer();
+
+        this.jumpscareManager.checkTurnAround(player);
 
         if (!(player instanceof CygnusPlayer cygnusPlayer)) return;
         if (!cygnusPlayer.hasBlockedSprinting()) return;
@@ -18,3 +27,4 @@ public final class CygnusPlayerTickListener implements Consumer<PlayerTickEvent>
         cygnusPlayer.sendPacket(cygnusPlayer.getMetadataPacket());
     }
 }
+
