3bd8b285db2d3bc2c48d7c0812ad77d8a529387f
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-04 22:11:07 +0200

    feat(spectator): add hotbar layout for spectator players

--- game/src/main/java/net/onelitefeather/cygnus/utils/Items.java
+++ game/src/main/java/net/onelitefeather/cygnus/utils/Items.java
@@ -1,12 +1,14 @@
 package net.onelitefeather.cygnus.utils;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.Player;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.onelitefeather.cygnus.common.Tags;
+import net.theevilreaper.aves.hotbar.HotBarLayout;
 
 /**
  * The class contains each {@link ItemStack} reference which is required for the game.
@@ -18,15 +20,36 @@ import net.onelitefeather.cygnus.common.Tags;
 @SuppressWarnings({"java:S3252"})
 public final class Items {
 
+    public static final byte SLENDER_ITEM = (byte) 0x00;
+    public static final byte SPECTATE_ITEM = (byte) 0x01;
+    public static final byte LEAVE_ITEM = (byte) 0x02;
+
     private static final ItemStack slenderEye = ItemStack.builder(Material.ENDER_EYE)
             .customName(Component.text("SlenderEye").color(TextColor.fromHexString("#ff00d4")))
-            .set(Tags.ITEM_TAG, (byte) 0)
+            .set(Tags.ITEM_TAG, SLENDER_ITEM)
             .build();
+
+    private static final HotBarLayout SPECTATOR_LAYOUT;
+
+    static {
+        SPECTATOR_LAYOUT = new HotBarLayout();
+        SPECTATOR_LAYOUT.set(2, ItemStack.builder(Material.COMPASS)
+                .customName(Component.text("Spectate"))
+                .set(Tags.ITEM_TAG, SPECTATE_ITEM)
+                .build()
+        );
+
+        SPECTATOR_LAYOUT.set(5, ItemStack.builder(Material.OAK_DOOR)
+                .customName(Component.text("Leave", NamedTextColor.RED))
+                .set(Tags.ITEM_TAG, LEAVE_ITEM)
+                .build()
+        );
+    }
 
     /**
      * Sets the {@link ItemStack} for the SlenderEye to the player inventory.
      *
-     * @param player the player who should receive the item
+     * @param player who should receive the item
      */
     public static void setSlenderEye(Player player) {
         player.getInventory().clear();
@@ -34,5 +57,16 @@ public final class Items {
         player.switchEntityType(EntityType.ENDERMAN);
     }
 
-    private Items() {}
+    /**
+     * Sets the spectator layout to a given {@link Player}.
+     *
+     * @param player to set the items
+     */
+    public static void setSpectatorLayout(Player player) {
+        SPECTATOR_LAYOUT.apply(player, true);
+    }
+
+    private Items() {
+        // Nothing do to here
+    }
 }
