7549957523cc558ba76d5a396d556710bb82cd87
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-05 22:21:14 +0200

    chore(game): update death handling

--- game/src/main/java/net/onelitefeather/cygnus/listener/PlayerDeathListener.java
+++ game/src/main/java/net/onelitefeather/cygnus/listener/PlayerDeathListener.java
@@ -3,25 +3,24 @@ package net.onelitefeather.cygnus.listener;
 import net.theevilreaper.xerus.api.phase.Phase;
 import net.theevilreaper.xerus.api.team.Team;
 import net.theevilreaper.xerus.api.team.TeamService;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
+import net.minestom.server.event.EventDispatcher;
 import net.minestom.server.event.player.PlayerDeathEvent;
 import net.onelitefeather.cygnus.common.Messages;
 import net.onelitefeather.cygnus.common.Tags;
+import net.onelitefeather.cygnus.common.config.GameConfig;
 import net.onelitefeather.cygnus.entity.DeadPlayerMannequin;
 import net.onelitefeather.cygnus.event.GameFinishEvent;
 import net.onelitefeather.cygnus.jumpscare.JumpScareManager;
 import net.onelitefeather.cygnus.phase.GamePhase;
-import net.onelitefeather.cygnus.team.TeamHelper;
+import net.onelitefeather.cygnus.player.event.SpectatorAddEvent;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public final class PlayerDeathListener implements Consumer<PlayerDeathEvent> {
 
-    private final Component kickMessage = Component.text("Thanks for playing it. <3", NamedTextColor.RED);
     private final Supplier<Phase> phaseSupplier;
     private final Team survivorTeam;
     private final Team slenderTeam;
@@ -29,8 +28,10 @@ public final class PlayerDeathListener implements Consumer<PlayerDeathEvent> {
 
     public PlayerDeathListener(Supplier<Phase> phaseSupplier, TeamService teamService, JumpScareManager jumpscareManager) {
         this.phaseSupplier = phaseSupplier;
-        this.survivorTeam = teamService.getTeams().get(TeamHelper.SURVIVOR_TEAM_ID);
-        this.slenderTeam = teamService.getTeams().get(TeamHelper.SLENDER_TEAM_ID);
+        this.survivorTeam = teamService.getTeam(GameConfig.SURVIVOR_KEY)
+                .orElseThrow(() -> new IllegalStateException("Survivor team not found"));
+        this.slenderTeam = teamService.getTeam(GameConfig.SLENDER_KEY)
+                .orElseThrow(() -> new IllegalStateException("Slender team not found"));
         this.jumpscareManager = jumpscareManager;
     }
 
@@ -48,7 +49,7 @@ public final class PlayerDeathListener implements Consumer<PlayerDeathEvent> {
         event.setChatMessage(Messages.getDeathComponent(player));
         survivorTeam.removePlayer(player);
         player.removeTag(Tags.TEAM_ID);
-        player.kick(kickMessage);
+        EventDispatcher.call(new SpectatorAddEvent(player));
         Phase currentPhase = this.phaseSupplier.get();
         //TODO: Should be tested
         if (!(currentPhase instanceof GamePhase gamePhase) || !survivorTeam.isEmpty()) return;
