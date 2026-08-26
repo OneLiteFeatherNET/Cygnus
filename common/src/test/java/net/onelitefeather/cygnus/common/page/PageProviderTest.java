55f38d9f6fee530d7b7d655240c6cd57cc894bad
onelitefeather <schauderkristall@hotmail.de>
2026-08-26 23:34:21 +0200

    chore(tests): set PageProviderTest to execute on the same thread for JUnit

--- common/src/test/java/net/onelitefeather/cygnus/common/page/PageProviderTest.java
+++ common/src/test/java/net/onelitefeather/cygnus/common/page/PageProviderTest.java
@@ -13,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -33,6 +35,7 @@ import java.util.stream.IntStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MicrotusExtension.class)
+@Execution(ExecutionMode.SAME_THREAD)
 class PageProviderTest {
 
     @Test
